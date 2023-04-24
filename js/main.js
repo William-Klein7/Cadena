686cfe9c5a938ddacc55ddeede78d123160b3cfd
William Teuschel Klein <williamklein08@gmail.com>
2023-04-24 17:27:04 -0300

    Projeto finalizado

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ darkMode.addEventListener("click", function () {
   html.classList.remove("light-mode");
   html.classList.add("dark-mode");
 });
-
 lightMode.addEventListener("click", function () {
   html.classList.remove("dark-mode");
   html.classList.add("light-mode");
